b0c7d43d1ba9a2a44cbe1e40af8f30f1f9dc43eb
Herman Malik <herman77malik@gmail.com>
2025-03-16 21:31:59 -0700

    fixed the code. wasn't working for multiple words.

--- main.py
+++ main.py
@@ -1,41 +1,51 @@
 import pyxhook
 import time
+import math
 
 timer = None
-
 last_press = None
-
 total_time = 0
 total_words = 0
+current_time = 0
+current_words = 0
 
 def OnKeyPress(event):
 	global timer
 	global last_press
 	global total_time
 	global total_words
+	global current_time
+	global current_words
 
-	if event.Ascii > 65 and event.Ascii < 122:
+	if event.Ascii > 32 and event.Ascii < 122:
 		if timer is None:
 			timer = time.time()
 			last_press = time.time()
 		elif time.time() - last_press <= 1:
 			last_press = time.time() #Reset timer
 		else:
-			#Reset word count
-			words = 1
+			wpm = (current_words * 60) / current_time
+			avg_wpm = (total_words * 60) / total_time
+			print(f"Typed {current_words} word(s) in {current_time:.2f} seconds.")
+			print(f"{wpm:.2f} WPM.")
+			print(f"Avg: {avg_wpm:.2f} WPM.")
+			# Reset word count
+			current_words = 0
 			timer = None
 	elif (event.Key == "Return" or chr(event.Ascii) == ' ') and timer is not None:
-		#Calculate wpm
-		deltaT = time.time() - timer
-		print("Typed 1 word in " + str(deltaT) + " seconds.")
-		print(str(60 / deltaT) +  " wpm.")
-
-		total_time += deltaT
-		total_words += 1
-
-		print("Avg: " + str(total_words * 60 / total_time))
-		timer = None
- 	
+		# Calculate wpm
+		current_words += 1
+		if timer is not None:
+			# Calculate WPM every time a space or enter is pressed
+			current_time = time.time() - timer
+			total_time += current_time
+			total_words += current_words
+		if current_words == int(math.sqrt(current_words)) ** 2: # perfect square
+			wpm = (current_words * 60) / current_time
+			avg_wpm = (total_words * 60) / total_time
+			print(f"Typed {current_words} word(s) in {current_time:.2f} seconds.")
+			print(f"{wpm:.2f} WPM.")
+			print(f"Avg: {avg_wpm:.2f} WPM.")
 
 h = pyxhook.HookManager()
 h.KeyDown = OnKeyPress
